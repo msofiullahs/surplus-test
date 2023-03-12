d21e01380dfbc373ec8d29dba98c68ce817ac9ca
Muhammad Sofiullah <sofiullah.work@gmail.com>
2023-03-12 22:29:33 +0700

    feature-category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function store($id = null, Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'name'      => 'string',
+            'enable'    => 'boolean'
+        ]);
+        if (empty($id)) {
+            $validate = Validator::make($request->all(), [
+                'name'      => 'required, string',
+                'enable'    => 'required, boolean'
+            ]);
+        }
+
+        if ($validate->fails()) {
+            return response()->json($validate->messages(), Response::HTTP_BAD_REQUEST);
+        }
+
+        if (!empty($id)) {
+            $category = Category::find($id)->update([
+                'name'=> $request->name,
+                'enable'=> $request->enable
+            ]);
+        } else {
+            $category = Category::create([
+                'name'=> $request->name,
+                'enable'=> $request->enable
+            ]);
+        }
+
+        return response()->json($category, Response::HTTP_CREATED);
+    }
+
+    public function delete($id)
+    {
+        $category = Category::find($id);
+        $category->products()->detach();
+        $category->delete();
+        return response()->json(['message'=>'Category is deleted'], Response::HTTP_ACCEPTED);
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'enable'
+    ];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'category_product', 'category_id', 'product_id');
+    }
 }
